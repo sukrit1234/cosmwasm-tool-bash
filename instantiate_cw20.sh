bbc1921d2664f7ca58246c51a34abd54051e4d90
Thirawat <sendsukrit@hotmail.com>
2022-12-20 14:16:13 +0700

    Update instantiate_cw20.sh

--- instantiate_cw20.sh
+++ instantiate_cw20.sh
@@ -65,6 +65,18 @@ if [[ $TOKEN_DECIMAL -lt 0 ]] || [[ $TOKEN_DECIMAL -gt 18 ]]; then
    exit 1
 fi 
 
+echo "Input Max Supply"
+read MAX_SUPPLY 
+if [[ -z $MAX_SUPPLY ]]; then
+   MAX_SUPPLY="100000000000000000000000000"
+   echo "No max supply is set default to  : " $MAX_SUPPLY " 18 decimal included"
+else
+   if ! [[ $MAX_SUPPLY =~ ^[0-9]+\.?[0-9]*$ ]]; then
+      echo "Error : Token max supply be unsigned integer"
+      exit 1
+   fi
+fi
+
 echo "Input instant label if leave this 'CW20Token' will be used"
 read INSTANCE_LABEL
 if [[ -z $INSTANCE_LABEL ]]; then
@@ -80,11 +92,11 @@ if WALLET_ADDRESS=$(wasmd keys show -a $2); then
    INSTANT_ARGS+=$TOKEN_SYMBOL
    INSTANT_ARGS+='","decimals":'
    INSTANT_ARGS+=$TOKEN_DECIMAL
-   INSTANT_ARGS+=',"initial_balances":[]'
-   INSTANT_ARGS+=',"mint":{"minter":"' 
-   INSTANT_ARGS+=$WALLET_ADDRESS
-   INSTANT_ARGS+='"}}'
-   
+   INSTANT_ARGS+=',"max_supply":"'
+   INSTANT_ARGS+=$MAX_SUPPLY
+   INSTANT_ARGS+='"}'
+
+
    if INIT_ARGS_JSON=$(jq -n --arg wallet $WALLET_ADDRESS "$INSTANT_ARGS"); then
       echo "About to instantiating the contract.. for CODE_ID : " $1 " WITH ADDRESS : " $WALLET_ADDRESS
       echo "Prepare Initial Json Data : "
