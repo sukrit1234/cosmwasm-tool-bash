8a75b8ff5855c2ea55c58cf93676bb79c59016a6
Thirawat <sendsukrit@hotmail.com>
2022-12-23 20:45:10 +0700

    Update instantiate_cw721.sh

--- instantiate_cw721.sh
+++ instantiate_cw721.sh
@@ -53,6 +53,21 @@ else
    fi
 fi 
 
+echo "Input Max Supply"
+read MAX_SUPPLY 
+if [[ -z $MAX_SUPPLY ]]; then
+   MAX_SUPPLY="10000"
+   echo "No max supply is set default to  : " $MAX_SUPPLY
+else
+   if ! [[ $MAX_SUPPLY =~ ^[0-9]+\.?[0-9]*$ ]]; then
+      echo "Error : Token max supply be unsigned integer"
+      exit 1
+   fi
+fi
+
+
+
+
 echo "Input instant label if leave this 'CW721Token' will be used"
 read INSTANCE_LABEL
 if [[ -z $INSTANCE_LABEL ]]; then
@@ -65,8 +80,10 @@ if WALLET_ADDRESS=$(wasmd keys show -a $2); then
    INSTANT_ARGS+=$TOKEN_NAME
    INSTANT_ARGS+='","symbol":"'
    INSTANT_ARGS+=$TOKEN_SYMBOL
-   INSTANT_ARGS+='"}'
-   
+   INSTANT_ARGS+='","max_supply":'
+   INSTANT_ARGS+=$MAX_SUPPLY
+   INSTANT_ARGS+='}'
+
    if INIT_ARGS_JSON=$(jq -n --arg wallet $WALLET_ADDRESS "$INSTANT_ARGS"); then
       echo "About to instantiating the contract.. for CODE_ID : " $1 " WITH ADDRESS : " $WALLET_ADDRESS
       echo "Prepare Initial Json Data : "
